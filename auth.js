f490271924255ae11f088370964384f340d8ff97
sharKar02 <157786925+sharKar02@users.noreply.github.com>
2024-08-06 09:41:38 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,25 +1,4 @@
-const jwt = require('jsonwebtoken');
-const secretKey = 'your_secret_key';
-
 function generateToken(user) {
-  return jwt.sign({ id: user.id }, secretKey, { expiresIn: '1h' });
-}
-
-function authenticateToken(req, res, next) {
-
-  const token = req.headers.authorization;
-//   const token = req.headers;
-  console.log({token});
-
-  if (token == null) return res.sendStatus(401);
-
-  jwt.verify(token, secretKey, (err, user) => {
-
-
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-}
-
-module.exports = { generateToken, authenticateToken };
+  return "token";
+} 
+module.exports = { generateToken };
